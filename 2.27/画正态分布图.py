d63d47c16ac294d5f32e4970f5d6f386c9c4ff08
2liuqianli2 <3047708553@qq.com>
2023-03-06 16:37:54 +0800

    第二次提交

---
+++ 2.27/画正态分布图.py
@@ -0,0 +1,5 @@
+import numpy as np
+import seaborn
+import matplotlib
+arr = np.random.normal(loc=175,scale=15,size=50)
+seaborn.violinplot(arr)
---
+++ 3.4/3.5.py
@@ -0,0 +1,5 @@
+import numpy as py
+import pandas as pd
+from pandas import Series,DataFrame
+df=pd.read_excel("./zh.xlsx")
+print(df)
