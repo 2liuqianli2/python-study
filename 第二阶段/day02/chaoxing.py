ad7c7b067073825b76c70055fc8bf017aad16881
2liuqianli2 <3047708553@qq.com>
2023-03-28 23:51:26 +0800

    3.28-23:51

---
+++ 第二阶段/day02/chaoxing.py
@@ -0,0 +1,19 @@
+import requests
+from lxml import etree
+url='http://i.chaoxing.com/base?t=1645608348941'
+
+header={
+'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36',
+
+'accept':'image/avif,image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8',
+'accept-encoding':'gzip, deflate, br',
+'accept-language':'zh-CN,zh;q=0.9',
+
+}
+
+data=requests.get(url,headers=header)
+
+data_xpath=etree.HTML(data.text)
+
+ke=data_xpath.xpath("//*[@id='c_233581519']/div[2]/text()")
+print(ke)
